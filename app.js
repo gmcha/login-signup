8ad811a9b8b716e021a4fd51865629a04e85e659
Ga Min Cha <prettygamin@naver.com>
2023-01-14 19:56:27 +0900

    Update: http로 서버 띄워보기

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("여기는 루트입니다.");
+const http = require("http");
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type" : "text/html; charset=utf-8"});
+    if(req.url === "/"){
+        res.end("여기는 루트 입니다.");
+    } else if (req.url === "/login"){
+        res.end("여기는 로그인 화면입니다.");
+    }
 });
 
-app.get("/login", (req, res)=> {
-    res.send("여기는 로그인 화면입니다.");
+app.listen(3001, () => {
+    console.log("http로 가동된 서버입니다.");
 });
 
-app.listen(3000, () => {
-    console.log("서버 가동");
-});
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//     res.send("여기는 루트입니다.");
+// });
+
+// app.get("/login", (req, res)=> {
+//     res.send("여기는 로그인 화면입니다.");
+// });
+
+// app.listen(3000, () => {
+//     console.log("서버 가동");
+// });
