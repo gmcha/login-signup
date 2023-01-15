08abea61a57993c4eb730d73aa4793a717f62161
Ga Min Cha <prettygamin@naver.com>
2023-01-15 22:10:13 +0900

    로그인(view) 최적화/ MVC의 V(view) 분리하기

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
 const express = require("express");
 const app = express();
 
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="ko">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            여기는 루트입니다.
-        </body>
-        </html>
-    `);
+    res.render("home/index");
 });
 
 app.get("/login", (req, res)=> {
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="ko">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <input type="text" placeholder="아이디"><br>
-            <input type="text" placeholder="비밀번호"><br>
-            <button>로그인</button>
-        </body>
-        </html>
-    `);
-    
+    res.render("home/login");
 });
 
 app.listen(3000, () => {
