23b8c6ee2bc2d26b6c2a316eb4147dae87d4cc8a
Ga Min Cha <prettygamin@naver.com>
2023-01-19 23:24:01 +0900

    Update: 라우팅 분리

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+"use strict";
+
+// 모듈
 const express = require("express");
 const app = express();
+
+const PORT = 3000;
+// 라우팅
+const home = require("./routes/home");
 
 // 앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("home/index");
-});
+app.use("/", home); // use -> 미들 웨어를 등록해 주는 메서드
 
-app.get("/login", (req, res)=> {
-    res.render("home/login");
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("서버 가동");
 });
