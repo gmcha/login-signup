870d8b5c0850e0f43a70dc76cdf613e01fa6657e
Ga Min Cha <prettygamin@naver.com>
2023-03-08 14:40:29 +0900

    Update: 서버의 응답데이터 처리 in 프런트

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -20,5 +20,13 @@ function login () {
         body: JSON.stringify(req),
     })
     .then((res) => res.json())
-    .then((res) => console.log(res)); // .then(console.log)해도 무방
+    .then((res) => {
+        if(res.success){
+            location.href = "/";
+        } else {
+            alert(res.msg);
+        }
+    }).catch((err) => {
+        console.error(new Error("로그인 중 에러 발생"));
+    });
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -29,10 +29,8 @@ const process = {
         
         return res.json({
             success : false,
-            message : "로그인에 실패하셨습니다.",
-        })
-
-        
+            msg : "로그인에 실패하셨습니다.",
+        });
     },
 };
 
