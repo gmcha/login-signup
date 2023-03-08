553ce2aec3bf3d13cf61b11af8b3e5c80d1ce875
Ga Min Cha <prettygamin@naver.com>
2023-03-08 13:53:56 +0900

    Update: 로그인 인증 기능 만들기 in 서버| 유저 데이터 만들기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,5 +18,7 @@ function login () {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+    .then((res) => res.json())
+    .then((res) => console.log(res)); // .then(console.log)해도 무방
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+    id : ["prettygamin", "gmcha0323", "gamiinnn"],
+    psword : ["123", "1234", "12345"],
+};
+
 const output = {
     hello : (req, res) => {
         res.render("home/index");
@@ -11,7 +16,23 @@ const output = {
 
 const process = {
     login : (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            psword = req.body.psword;
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success : true,
+                });
+            };
+        };
+        
+        return res.json({
+            success : false,
+            message : "로그인에 실패하셨습니다.",
+        })
+
+        
     },
 };
 
